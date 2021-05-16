aca7a75aea53c9fda9a595280afccd0e977b5029
Arthur Taillez <arthur.taillez@outlook.com>
2021-05-16 17:41:26 +0200

    last push

---
+++ tests/QuestionsControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Tests\Controller;
+
+use App\Repository\UsersRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class QuestionsControllerTest extends WebTestCase
+{
+    public function testVisitingWhileNotLoggedIn()
+    {
+        $client = static::createClient();
+       
+        $client->request('GET', '/questions/');
+        $this->assertResponseRedirects();
+    }
+
+    public function testVisitingWhileLoggedIn()
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UsersRepository::class);
+
+        // retrieve the test user
+        $testUser = $userRepository->findOneByEmail('antoine@demo.fr');
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        // test e.g. the profile page
+        $client->request('GET', '/questions/');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Mes questions');
+        
+    }
+}
