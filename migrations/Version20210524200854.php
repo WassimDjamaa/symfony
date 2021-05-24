c4eae39b61b920f901a6a5e5a19f2209f07993e5
Wassim DJAMAA <56625264+WassimDjamaa@users.noreply.github.com>
2021-05-24 22:14:42 +0200

    correction bug in project symfony

--- migrations/Version20210524200854.php
+++ migrations/Version20210524200854.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210516145525 extends AbstractMigration
+final class Version20210524200854 extends AbstractMigration
 {
     public function getDescription(): string
     {
